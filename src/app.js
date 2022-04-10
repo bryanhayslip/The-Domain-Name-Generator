26c3550db9da640b5f55861cb08e8ad7a33a154a
bryanhayslip <bryan.hayslip@gmail.com>
2022-04-10 21:10:14 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,21 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let myDomain = function generateDomain() {
+  let pronoun = ["the", "my", "her", "their", "his"];
+  let subject = ["kitchen", "cat", "umbrella", "mouse", "basket"];
+  let description = ["game", "adventure", "challenge", "journey", "issue"];
+
+  let pronounIndex = Math.floor(Math.random() * pronoun.length);
+  let subjectIndex = Math.floor(Math.random() * subject.length);
+  let descriptionIndex = Math.floor(Math.random() * description.length);
+
+  return (
+    pronoun[pronounIndex] +
+    subject[subjectIndex] +
+    description[descriptionIndex] +
+    ".com"
+  );
 };
+
+console.log(myDomain);
